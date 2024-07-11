b5735f478a23bb4cf0e3f3ecfb5e16058d7ed9ca
LydiaBloomfield <lydia.r.bloom@gmail.com>
2024-07-11 17:15:37 -0400

    MVP of projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,8 @@ import "./Projects.css"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ME from "../assets/ME.png"
-import me from "../assets/mee.png"
+import fish from "../assets/fish.png"
+import carryOn from "../assets/carryOn.png"
 import { CardImage } from "react-bootstrap-icons";
 
 export const Projects = () => {
@@ -12,39 +13,36 @@ export const Projects = () => {
             <h1>Projects</h1>
             <div className="card-container">
                 <Card className="card">
-                    <img className="project-image-1" src={ME}/>
+                    <img className="project-image-1" src={carryOn} />
                     <Card.Body className="body">
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
+                    <h3>Carry On</h3>
+                    <Card.Text>
+                            A full-stack travel application designed for all of your potential travel needs. Functionality includes a financial planner, packing list, agenda, and map of previous destinations. Built with a MERN stack.
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
                     </Card.Body>
                 </Card>
                 <Card className="card">
-                    <Card.Img className="project-image-2" variant="top" src={ME} />
+                    <Card.Img className="project-image-2" variant="top" src={fish} />
                     <Card.Body className="body">
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
+                        <h3>F.I.S.H.</h3>
+                        <Card.Text className="card-text">
+                            A winning project from State Farm's 2024 Hack Day. Full-stack mapping application built from Python and Svelte. Project content and code is confidential.
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        {/* <Button className= "button" variant="primary">Go somewhere</Button> */}
                     </Card.Body>
                 </Card>
                 <Card className="card">
                     <Card.Img className="project-image-3" variant="top" src={ME} />
                     <Card.Body className="body">
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
+                        <h3> My Portfolio </h3>
+                        <Card.Text className="card-text">
+                            Project-ception! This portfolio site has been a lesson in JavaScript, HTML, and CSS as all styling and composition has been done by hand.
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
                     </Card.Body>
                 </Card>
-                
+
             </div>
         </>
     );
